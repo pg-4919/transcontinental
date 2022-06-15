4ddb5b8d473fe6c53261b057e40bdf731dc40466
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-06-15 08:19:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,26 @@ let lockGeneration = false;
 let month = 0;
 let negativeMeter = 0;
 let positiveMeter = 0;
+let currentEvent;
 
 function evaluateGeneric(vals, fn) {
     return fn.apply(null, vals);
 }
+
+const positiveEventsImmigrant = [
+    {
+        text: "Your team gets ahold of higher quality nitroglycerine, speeding up your progress through the mountains.<br><br>Gain 5 miles.",
+        rails: 5
+    },
+    {
+        text: "The weather is wonderful this month, and you're able to lay down track faster without it constantly snowing.<br><br>Gain 7 miles.",
+        rails: 7
+    },
+    {
+        text: "You get an extra supply of food, helping keep you energized while working.<br><br>Gain 3 miles.",
+        rails: 3
+    },
+]
 
 const negativeEventsImmigrant = [
     {
@@ -31,36 +47,75 @@ const negativeEventsImmigrant = [
         rails: 0
     },
     {
-        text: "One of your explosive crates went off, damaging the track as well as injuring your workers.<br><br>Lose 1 month and 1 mile of rails.",
+        text: "One of your explosive crates went off, damaging the track as well as injuring workers.<br><br>Lose 1 month and 5 miles of rails.",
         time: 1,
-        rails: 1
+        rails: 5
+    },
+];
+
+const positiveEventsVeteran = [
+    {
+        text: "You make a treaty with native tribes, leaving you to lay tracks unhindered.<br><br>Gain 5 miles.",
+        rails: 5
+    },
+    {
+        text: "The weather is excellent this month, and you're able to work faster without constantly being inside a thunderstorm.<br><br>Gain 7 miles.",
+        rails: 7
+    },
+    {
+        text: "A new batch of workers joins your team.<br><br>Gain 3 miles.",
+        rails: 3
     },
 ]
+
+const negativeEventsVeteran = [
+    {
+        text: "Native Americans tear up a section of your track.<br><br>Lose 10 miles.",
+        time: 0,
+        rails: 10
+    },
+    {
+        text: "A Native American raid steals most of your supplies.<br><br>Lose 1 month.",
+        time: 1,
+        rails: 0
+    },
+    {
+        text: "Your progress is interrupted by a massive buffalo herd which will not budge.<br><br>Lose 2 months.",
+        time: 2,
+        rails: 0
+    },
+    {
+        text: "A tornado forms over your track! It destroys the railroad as well as necessary supplies.<br><br>Lose 2 months and 15 miles of rails.",
+        time: 2,
+        rails: 15
+    },
+]
+
 
 const questions_4 = [
     {
         question: "When did construction of the railroad begin?",
-        choices: [ "1862", "1863", "1864", "1865" ],
+        choices: ["1862", "1863", "1864", "1865"],
         correct: 2
     },
     {
         question: "When did construction of the railroad conclude?",
-        choices: [ "1812", "1859", "1869", "1879" ],
+        choices: ["1812", "1859", "1869", "1879"],
         correct: 3
     },
     {
         question: "Where did the two rail lines meet?",
-        choices: [ "Stone", "Howell", "Plymouth", "Promontory" ],
+        choices: ["Stone", "Howell", "Plymouth", "Promontory"],
         correct: 4
     },
     {
         question: "What kind of spike was used to complete the railroad?",
-        choices: [ "Bronze", "Silver", "Gold", "Platinum" ],
+        choices: ["Bronze", "Silver", "Gold", "Platinum"],
         correct: 3
     },
     {
         question: "How much would the railroad cost if it were built today?",
-        choices: [ "1.3 billion", "130 million", "13 million", "1.3 million" ],
+        choices: ["1.3 billion", "130 million", "13 million", "1.3 million"],
         correct: 1
     },
     {
@@ -69,7 +124,7 @@ const questions_4 = [
             "Removed Native Americans from land needed by the railroad",
             "Provided medicine to all railroad workers",
             "Legalized killing Native Americans",
-            "Forced Native Americans to work on the railroad" ],
+            "Forced Native Americans to work on the railroad"],
         correct: 1
     },
     {
@@ -84,32 +139,23 @@ const questions_4 = [
     },
     {
         question: "How many years did it take to build the railroad?",
-        choices: [ "5", "6", "7", "8" ],
+        choices: ["5", "6", "7", "8"],
         correct: 2
     },
     {
         question: "Which president approved the railroad?",
-        choices: [ "James Buchanan", "Andrew Jackson", "Ulysses S. Grant", "Abraham Lincoln" ],
+        choices: ["James Buchanan", "Andrew Jackson", "Ulysses S. Grant", "Abraham Lincoln"],
         correct: 4
     },
     {
         question: "How did Native Americans react to the railroad?",
         choices: [
             "Taking the issue to the Supreme Court",
-            "Sabotaging the railroad and attacking white settlements", "Helping provide supplies and food to the builders",
+            "Sabotaging the railroad and attacking white settlements",
+            "Helping provide supplies and food to the builders",
             "Staying neutral on the condition that the railroad not disrupt their way of life."
         ],
         correct: 2
-    },
-    {
-        question: "How did Native Americans react to the railroad?",
-        choices: [
-            "Taking the issue to the Supreme Court",
-            "Helping provide supplies and food to the builders",
-            "Sabotaging the railroad and attacking white settlements",
-            "Staying neutral on the condition that the railroad not disrupt their way of life."
-        ],
-        correct: 3
     },
     {
         question: "How much were Chinese workers paid?",
@@ -173,7 +219,7 @@ const questions_4 = [
     },
     {
         question: "When was the Chinese Exclusion Act passed?",
-        choices: [ "1882", "1883", "1884", "1885" ],
+        choices: ["1882", "1883", "1884", "1885"],
         correct: 1
     },
 ];
@@ -181,57 +227,57 @@ const questions_4 = [
 const questions_2 = [
     {
         question: "Which railroad company started on the east?",
-        choices: [ "Union Pacific", "Central Pacific" ],
+        choices: ["Union Pacific", "Central Pacific"],
         correct: 2
     },
     {
         question: "Which railroad company started on the west?",
-        choices: [ "Union Pacific", "Central Pacific" ],
+        choices: ["Union Pacific", "Central Pacific"],
         correct: 1
     },
     {
         question: "Which railroad company mainly employed the Irish?",
-        choices: [ "Union Pacific", "Central Pacific" ],
+        choices: ["Union Pacific", "Central Pacific"],
         correct: 1
     },
     {
         question: "Which railroad company mainly employed the Chinese?",
-        choices: [ "Union Pacific", "Central Pacific" ],
+        choices: ["Union Pacific", "Central Pacific"],
         correct: 2
     },
     {
         question: "Were the majority of workers on the railroad immigrants?",
-        choices: [ "Yes", "No" ],
+        choices: ["Yes", "No"],
         correct: 1
     },
     {
         question: "Which railroad company ran into issues with Native Americans?",
-        choices: [ "Union Pacific", "Central Pacific" ],
+        choices: ["Union Pacific", "Central Pacific"],
         correct: 1
     },
     {
         question: "Which railroad company ran into issues with the local terrain?",
-        choices: [ "Union Pacific", "Central Pacific" ],
+        choices: ["Union Pacific", "Central Pacific"],
         correct: 2
     },
     {
         question: "After the railroad companies finished construction, did they stop building railroads?",
-        choices: [ "Yes", "No" ],
+        choices: ["Yes", "No"],
         correct: 2
     },
     {
         question: "Which railroad company had to build 690 miles of track?",
-        choices: [ "Union Pacific", "Central Pacific" ],
+        choices: ["Union Pacific", "Central Pacific"],
         correct: 2
     },
     {
         question: "Which railroad company had to build 1086 miles of track?",
-        choices: [ "Union Pacific", "Central Pacific" ],
+        choices: ["Union Pacific", "Central Pacific"],
         correct: 1
     },
     {
         question: "Is the railroad still in operation today?",
-        choices: [ "Yes", "No" ],
+        choices: ["Yes", "No"],
         correct: 1
     },
 ]
@@ -249,7 +295,7 @@ const dialogues = {
     },
     learn: {
         confirm: true,
-        text: "The year is 1863, and the country is in shambles. The Civil War rages between the North and the South. Despite this, dreams of uniting the East and West move the Central Pacific and Union Pacific Railroad Companies to begin construction on a great Transcontinental Railroad. For six long years, they would forge forward from Sacramento and Omaha to meet at Promontory, Utah.<br><br>This game was based on The Oregon Trail, by Bill Heinemann, Don Rawitsch, and Paul Dillenberger. It was coded by Peter Gong and Nithin Vinothkumar.<br><br>Due to the nature of the code, it can be easily modded. If you want to mod the game, all we ask is credit.<br><br><br>Press ENTER to continue.",
+        text: "The year is 1863, and the country is in shambles. The Civil War rages between the North and the South. Despite this, dreams of uniting the East and West move the Central Pacific and Union Pacific Railroad Companies to begin construction on a great Transcontinental Railroad. For six long years, they would forge forward from Sacramento and Omaha to meet at Promontory, Utah.<br><br>This game was based on The Oregon Trail, by Bill Heinemann, Don Rawitsch, and Paul Dillenberger. It was coded by Peter Gong and Nithin Vinothkumar.<br><br>Due to the nature of the code, it can be easily modded. If you want to mod the game, all we ask is credit.<br><br>Huge thanks to Pix (https://www.youtube.com/c/Pix-music) for the music.<br><br><br>Press ENTER to continue.",
         process: input => "opening",
         valid: char => false
     },
@@ -323,26 +369,20 @@ const dialogues = {
     immigrant_month: {
         template: true,
         confirm: true,
-        text: "`You are currently at month ${month}, with ${trackLength} miles done. You have ${690 - trackLength} to go.<br><br>CA ${generateTrackProgress(690)}<br><br><br>Press ENTER to continue.`",
+        text: "`You are currently at month ${month}, with ${trackLength} miles done. You have ${((690 - trackLength) > 0) ? 690 - trackLength : 'no more'} to go.<br><br>CA ${generateTrackProgress(690)}<br><br><br>Press ENTER to continue.`",
         process: input => {
-            if (Math.random())
-            if (Math.random() > 0.33) return "immigrant_question_4";
-            else return "immigrant_question_2";
+            if (trackLength >= 690) return "end";
+            else return generateEventImmigrant();
         },
         valid: char => false
     },
     immigrant_event: {
         template: true,
         confirm: true,
-        text: "`${generateNegativeEventImmigrant()}`",
-        process: input => true,
+        text: "`${currentEvent.text}<br><br><br>Press ENTER to continue.`",
+        process: input => "immigrant_question_4",
         valid: char => false
     },
-
-
-
-
-
     veteran_name: {
         text: "What is your name? ",
         process: input => {
@@ -407,40 +447,80 @@ const dialogues = {
     veteran_month: {
         template: true,
         confirm: true,
-        text: "`You are currently at month ${month}, with ${trackLength} miles done. You have ${1086 - trackLength} to go.<br><br>CA ${generateTrackProgress(1086)}<br><br><br>Press ENTER to continue.`",
+        text: "`You are currently at month ${month}, with ${trackLength} miles done. You have ${1086 - trackLength} to go.<br><br>NE ${generateTrackProgress(1086)}<br><br><br>Press ENTER to continue.`",
         process: input => {
-            if (Math.random() > 0.25) return "veteran_question_4";
-            else return "veteran_question_2";
+            if (trackLength >= 1086) return "end";
+            else return generateEventVeteran();
         },
         valid: char => false
+    },
+    veteran_event: {
+        template: true,
+        confirm: true,
+        text: "`${currentEvent.text}<br><br><br>Press ENTER to continue.`",
+        process: input => "veteran_question_4",
+        valid: char => false
+    },
+    end: {
+        confirm: true,
+        template: true,
+        text: "`You completed your side of the railroad in ${month} months, and reached Promontory Point, Utah! (Keep in mind, you went twice as fast as the real railroad companies.)<br><br> Thank you for playing the Transcontinental Railroad.<br><br><br>Press ENTER to play again.`",
+        process: input => {
+            window.location.reload();
+        }
     },
 }
 
 function weightedRand(spec) {
-    var i, j, table=[];
+    let i, j, table = [];
     for (i in spec) {
-        // The constant 10 below should be computed based on the
-        // weights in the spec for a correct and optimal table size.
-        // E.g. the spec {0:0.999, 1:0.001} will break this impl.
-        for (j=0; j<spec[i]*10; j++) {
-            table.push(i);
-        }
+        for (j = 0; j < spec[i] * 10; j++) table.push(i);
     }
-    return function() {
-        return table[Math.floor(Math.random() * table.length)];
-    }
+    return table[Math.floor(Math.random() * table.length)];
 }
 
 function generateEventImmigrant() {
-    if (positiveMeter > 6) {
-        
+    positiveMeter += parseInt(weightedRand({ 0: 0.7, 1: 0.3 }));
+    negativeMeter += parseInt(weightedRand({ 0: 0.7, 1: 0.3 }));
+
+    if (positiveMeter > 8) {
+        currentEvent = positiveEventsImmigrant[Math.floor(Math.random() * 3)]
+        trackLength += currentEvent.rails;
+        positiveMeter = 0;
+        return "immigrant_event";
     }
     if (negativeMeter > 5) {
-        const currentEvent = negativeEventsImmigrant[Math.floor(Math.random() * 4)];
+        currentEvent = negativeEventsImmigrant[weightedRand({ 0: 4, 1: 3, 2: 2, 3: 1 })];
         trackLength -= currentEvent.rails;
-        months += currentEvent.time;
-        return `${currentEvent.text}<br><br><br>Press ENTER to continue.`;
+        month += currentEvent.time;
+        negativeMeter = 0;
+        return "immigrant_event";
     }
+
+    if (Math.random() > 0.33) return "immigrant_question_4";
+    else return "immigrant_question_2";
+}
+
+function generateEventVeteran() {
+    positiveMeter += parseInt(weightedRand({ 0: 0.7, 1: 0.3 }));
+    negativeMeter += parseInt(weightedRand({ 0: 0.7, 1: 0.3 }));
+
+    if (positiveMeter > 8) {
+        currentEvent = positiveEventsVeteran[Math.floor(Math.random() * 3)]
+        trackLength += currentEvent.rails;
+        positiveMeter = 0;
+        return "veteran_event";
+    }
+    if (negativeMeter > 5) {
+        currentEvent = negativeEventsVeteran[weightedRand({ 0: 4, 1: 3, 2: 2, 3: 1 })];
+        trackLength -= currentEvent.rails;
+        month += currentEvent.time;
+        negativeMeter = 0;
+        return "veteran_event";
+    }
+
+    if (Math.random() > 0.33) return "veteran_question_4";
+    else return "veteran_question_2";
 }
 
 function generateTrackProgress(total) {
